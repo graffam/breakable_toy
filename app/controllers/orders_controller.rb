eea077ab6526b182d23994b228a6a55bf2d2116f
graffam <graffam87@gmail.com>
2015-01-11 20:19:36 -0500

    adds the ability to order reagents, passing tests

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,8 +11,23 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
-
+    @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    if @order.save
+      KitOrder.make_items(params[:order][:kit_orders_attributes], @order)
+      flash[:notice] = "Order Created Successfully"
+      redirect_to order_path(@order)
+    end
   end
 
+  def show
+    @order = Order.find(params["id"])
+    @kit_orders = @order.kit_orders
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:machine_id, :comment, :needed_by)
+  end
 end
--- spec/features/order/user_places_order_spec.rb
+++ spec/features/order/user_places_order_spec.rb
@@ -6,10 +6,10 @@ feature 'placing order', %Q{
   So that I can receive reagent kits
   } do
 # Acceptance Criteria:
-# [] Be able to place an order for reagents
+# [X] Be able to place an order for reagents
 # [] I Cannot order reagents for the wrong machine
 # [] I must be logged in to order
-# [] I should see a message saying my order has been placed
+# [X] I should see a message saying my order has been placed
 # [] After placing the order, i'm brought to the order details
 
   context "User is signed in" do
@@ -31,14 +31,14 @@ feature 'placing order', %Q{
 
       click_button "Submit"
 
-      save_and_open_page
       fill_in "Comment", with: "Need this asap"
       fill_in "order[kit_orders_attributes][0][amount]", with: "1"
+      fill_in "order_needed_by", with:"07/10/2015"
 
       click_button "Submit"
 
-      expect(page).to have_content("Order Placed")
-      expect(page).to have_content("168c 1")
+      expect(page).to have_content("Order Created Successfully")
+      expect(page).to have_content("168c: 1")
       expect(page).to have_content("Need this asap")
     end
   end

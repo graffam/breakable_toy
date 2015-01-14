aa976628192a5803d8c9e19ed7c57ab2e24dd5e0
graffam <graffam87@gmail.com>
2015-01-14 18:36:58 -0500

    adds ability for admin to edit users information

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class UsersController < ApplicationController
     before_filter :authorize!
+
     def index
       @users = User.all
     end
@@ -9,11 +10,32 @@ module Admin
       @user = User.find(params[:id])
     end
 
+    def edit
+      @user = User.find(params[:id])
+    end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_update_params)
+        flash[:notice] = "User Successfully Updated"
+        redirect_to admin_user_path(@user)
+      else
+        flash[:notice] = "User Not Updated"
+        render "edit"
+      end
+    end
+
     def destroy
       @user = User.find(params[:id])
       @user.destroy
       flash[:notice] = "User #{@user.email} deleted"
       redirect_to admin_users_path
+    end
+
+    protected
+
+    def user_update_params
+      params.require(:user).permit(:email, :first_name, :last_name)
     end
   end
 end

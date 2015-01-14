6fd0b24e4501c81bf7cb0758cb88dbc478e4b616
graffam <graffam87@gmail.com>
2015-01-14 10:24:37 -0500

    adds namespace for admins, limites resoucres for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :orders
+  resources :orders, only: [:index, :show, :edit, :update, :create, :new]
+
+  namespace :admin do
+    resources :orders
+  end
 end

5cc09a059a25a8f8fb9ef9f818ce42d4d1aff00d
graffam <graffam87@gmail.com>
2015-01-14 13:12:56 -0500

    adds admin controller for users, adds admin authentication for admins pages

--- spec/features/admin/admin_views_users_spec.rb
+++ spec/features/admin/admin_views_users_spec.rb
@@ -9,22 +9,47 @@ feature 'admin user functions', %{
   so that I may better manage my application
   } do
     # Acceptance Criteria:
-    # [] I must see a list of all the users
-    # [] be able to click on the user to view their details
+    # [X] I must see a list of all the users
+    # [X] be able to click on the user to view their details
     # [] I must have admin rights to view users
-    # [] I should be able to sort users by name, date joined, and email
+    # [X] I should be able to sort users by name, date joined, and email
 
   let(:admin) { FactoryGirl.create(:user, role: 'admin') }
   let!(:user) { FactoryGirl.create(:user) }
 
-  scenario "admin visits index page of users" do
-    login_as(admin)
+  context "admin is signed in" do
+    before(:each) do
+      login_as(admin)
+    end
+
+    scenario "admin visits index page of users" do
+      visit admin_users_path
+
+      expect(page).to have_content(user.email)
+      expect(page).to have_content(user.first_name)
+      expect(page).to have_content(user.last_name)
+      expect(page).to have_content(user.created_at)
+    end
+
+    scenario "admin visits details page of user" do
+      visit admin_users_path
+
+      click_on ("#{user.id}")
+
+      expect(page).to have_content(user.email)
+      expect(page).to have_content(user.first_name)
+      expect(page).to have_content(user.last_name)
+      expect(page).to have_content(user.created_at)
+      expect(page).to have_button("Edit User")
+    end
+  end
+
+  scenario "unathorized users are redirected" do
+    login_as(user)
 
     visit admin_users_path
 
-    expect(page).to have_content(user.email)
-    expect(page).to have_content(user.first_name)
-    expect(page).to have_content(user.last_name)
-    expect(page).to have_content(user.date_created)
+    expect(page).to have_content("not authorized")
+    expect(current_path).to eq(root_path)
   end
 end

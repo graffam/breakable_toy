430de6bf8d3d973ec0186f71f942ce7de54aea61
graffam <graffam87@gmail.com>
2015-01-22 00:26:29 -0500

    fixes stylistic issues

--- app/helpers/active_helper.rb
+++ app/helpers/active_helper.rb
@@ -1,6 +1,6 @@
 module ActiveHelper
-  def is_active?(controller,page_name)
-    "active" if params[:action] == page_name && 
+  def is_active?(controller, page_name)
+    "active" if params[:action] == page_name &&
                 params[:controller] == controller
   end
 
--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,12 +1,12 @@
 class Metric
   def self.prepare(*query)
     data = { "labels" => [],
-      "datasets" => [
-        fillColor: "rgb(1,97,158)",
-        strokeColor: "rgba(220,220,220,0.8)",
-        highlightFill: "rgba(220,220,220,0.75)",
-        highlightStroke: "rgba(220,220,220,1)",
-        data: []] }
+             "datasets" => [
+                fillColor: "rgb(1,97,158)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: []] }
     machines = Machine.all
     if query[0] == "today"
       machines.each do |key|
